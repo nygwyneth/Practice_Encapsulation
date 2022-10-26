fa780221ab9c5177f1719348c8e4487414fafb5a
nygwyneth <nuga.shaniagwyneth@gmail.com>
2022-10-26 09:44:21 +0800

    first commit

---
+++ src/Card.java
@@ -0,0 +1,43 @@
+class Card {
+    
+    private int balanceAmount, withdrawAmount, cardNumber;
+    private int defaultMoney = 1000;
+    
+    public int getBalanceAmount() {
+        return balanceAmount = 1000;
+    }
+    
+    public int getWithdrawAmount() {
+        return withdrawAmount;
+    }
+    
+    public int getCardNumber() {
+        return cardNumber = 92621083;
+    }
+    
+    public void setBalanceAmount (int newBalanceAmount) {
+        balanceAmount = newBalanceAmount;
+    }
+    
+    public void setWithdrawAmount (int newWithdrawAmount) {
+        withdrawAmount = newWithdrawAmount;
+    }
+    
+    public void calculateBalance() {
+        int totalBal = defaultMoney + balanceAmount;
+        System.out.println("Total Balance: Php " + totalBal + "\n\n----- Deposit Success! -----");
+    }
+    
+    public void withdrawMoney() {
+        if (withdrawAmount <= getBalanceAmount()) {
+            if (withdrawAmount%100 == 0) {
+                int withdrawTransaction = balanceAmount - withdrawAmount;
+                System.out.println("\n\n----- Withdraw Success! -----\nRemaining Balance: Php " + withdrawTransaction);
+            } else {
+            System.out.println("Error! Please try again.");
+            }
+        } else {
+            System.out.println("Error! Invalid amount.");
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+class Main {
+    
+    static Scanner in = new Scanner(System.in);
+    static Card card = new Card();
+    static Main main = new Main();
+    
+    String options = "\n(0) Exit \n(1) Display Card Information \n(2) Deposit \n(3) Withdraw";
+    int choice, deposit, withdraw;
+    
+    public static void main(String[] args) {
+        main.startTransaction();
+    }
+    
+    public void startTransaction() {
+        System.out.println("\nChoose from the following to start transaction." + options + "\n");
+        System.out.print("Enter number here (0-3): ");
+        choice = in.nextInt();
+        
+        if (choice > 3 && choice < 0) {
+            System.out.println("Error! Please try again.");
+        } else if (choice == 0) {
+            System.out.println("\n----- End of transaction -----");
+        } else if (choice == 1) {
+            main.cardNumber();
+            System.out.println("Balance Amount: Php " + card.getBalanceAmount() + "\n\n----- End of transaction -----");
+        } else if (choice == 2) {
+            main.balance();
+        } else if (choice == 3) {
+            main.withdraw();
+        }
+    }
+    
+    public void cardNumber() {
+        System.out.println("\nCard Number: " + card.getCardNumber());
+    }
+    
+    public void balance() {
+        System.out.print("\nEnter Amount to Deposit: Php ");
+        deposit = in.nextInt();
+        
+        card.setBalanceAmount(deposit);
+        card.calculateBalance();
+    }
+    
+    public void withdraw() {
+        System.out.print("\nEnter Amount to Withdraw: Php ");
+        withdraw = in.nextInt();
+        
+        card.setWithdrawAmount(withdraw);
+        card.withdrawMoney();
+    }
+}
